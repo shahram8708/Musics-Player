22d29b63c7cb5e83ae367c188076c8437ca3333e
Shah Ram <152763547+shahram8708@users.noreply.github.com>
2024-05-19 09:57:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+JAMENDO_API_URL = "https://api.jamendo.com/v3.0/tracks"
+JAMENDO_API_KEY = "ab243d27"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search/<query>')
+def search(query):
+    params = {
+        'client_id': JAMENDO_API_KEY,
+        'format': 'json',
+        'limit': 10,
+        'namesearch': query,
+        'tags': 'hindi',  
+        'order': 'popularity_total'
+    }
+    response = requests.get(JAMENDO_API_URL, params=params)
+    data = response.json()
+    return jsonify(data['results'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
